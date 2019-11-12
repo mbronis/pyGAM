3876d4cc20c31cbc425ef42495a969f0f70b241f
mbronis <michal.bronis@gmail.com>
2019-11-12 22:50:03 +0100

    proper split of quick start and a tour

--- 02 a tour of pyGAM.py
+++ 02 a tour of pyGAM.py
@@ -1,11 +1,16 @@
+
+############################################################
 # https://pygam.readthedocs.io/en/latest/notebooks/tour_of_pygam.html
 
 
-# interaction and 3d plot
+
+#Fitting and plotting interactions with te()
+
 from pygam import PoissonGAM, s, te
 from pygam.datasets import chicago
 
 X, y = chicago(return_X_y=True)
+X.shape
 
 gam = PoissonGAM(s(0, n_splines=200) + te(3, 1) + s(2)).fit(X, y)
 
@@ -20,5 +25,178 @@ Z = gam.partial_dependence(term=1, X=XX, meshgrid=True)
 
 ax = plt.axes(projection='3d')
 ax.plot_surface(XX[0], XX[1], Z, cmap='viridis')
+
+
+#Simple interactions, copare with te()
+
+from pygam import LinearGAM, s
+from pygam.datasets import toy_interaction
+
+X, y = toy_interaction(return_X_y=True)
+
+gam = LinearGAM(s(0, by=1)).fit(X, y)
+gam.summary()
+
+
+gam1 = LinearGAM(s(0)+s(1)).fit(X, y)
+gam1.summary()
+
+
+gam2 = LinearGAM(te(0,1)).fit(X, y)
+gam2.summary()
+
+
+import pandas as pd
+pd.DataFrame(X).corr()
+
+
+######################################################
+# regression
+
+from pygam import LinearGAM, s, f
+from pygam.datasets import wage
+
+X, y = wage(return_X_y=True)
+
+## model
+gam = LinearGAM(s(0) + s(1) + f(2))
+gam.gridsearch(X, y)
+gam.summary()
+
+
+## plotting
+plt.figure()
+fig, axs = plt.subplots(1,3)
+
+titles = ['year', 'age', 'education']
+for i, ax in enumerate(axs):
+    XX = gam.generate_X_grid(term=i)
+    ax.plot(XX[:, i], gam.partial_dependence(term=i, X=XX))
+    ax.plot(XX[:, i], gam.partial_dependence(term=i, X=XX, width=.95)[1], c='r', ls='--')
+    if i == 0:
+        ax.set_ylim(-30,30)
+    ax.set_title(titles[i])
+
+
+######################################################
+# classification
+
+
+from pygam import LogisticGAM, s, f
+from pygam.datasets import default
+
+X, y = default(return_X_y=True)
+X.shape
+X
+
+gam = LogisticGAM(f(0) + s(1) + s(2)).gridsearch(X, y)
+
+fig, axs = plt.subplots(1, 3)
+titles = ['student', 'balance', 'income']
+
+for i, ax in enumerate(axs):
+    XX = gam.generate_X_grid(term=i)
+    pdep, confi = gam.partial_dependence(term=i, width=.95)
+
+    ax.plot(XX[:, i], pdep)
+    ax.plot(XX[:, i], confi, c='r', ls='--')
+    ax.set_title(titles[i])
+
+gam.accuracy(X, y)
+
+######################################################
+# constraints
+
+
+
+from pygam import LinearGAM, s
+from pygam.datasets import hepatitis
+
+X, y = hepatitis(return_X_y=True)
+X.shape
+
+
+gam1 = LinearGAM(s(0, constraints='monotonic_inc')).fit(X, y)
+gam2 = LinearGAM(s(0, constraints='concave')).fit(X, y)
+
+fig, ax = plt.subplots(1, 2)
+ax[0].plot(X, y, label='data')
+ax[0].plot(X, gam1.predict(X), label='monotonic fit')
+ax[0].legend()
+
+ax[1].plot(X, y, label='data')
+ax[1].plot(X, gam2.predict(X), label='concave fit')
+ax[1].legend()
+
+
+
+
+######################################################
+# api
+
+
+
+from pygam import LogisticGAM, s, f
+from pygam.datasets import toy_classification
+
+X, y = toy_classification(return_X_y=True, n=5000)
+
+gam = LogisticGAM(s(0) + s(1) + s(2) + s(3) + s(4) + f(5))
+gam.fit(X, y)
+gam.summary()
+
+plt.figure()
+for i, term in enumerate(gam.terms):
+    if term.isintercept:
+        continue
+    plt.plot(gam.partial_dependence(term=i))
+
+
+######################################################
+# callbacks
+
+
+gam = plt.plot(gam.logs_['deviance'])
+
+
+
+######################################################
+# histogram smoothing
+
+
+from pygam import PoissonGAM
+from pygam.datasets import faithful
+
+X, y = faithful(return_X_y=True)
+
+
+
+gam = PoissonGAM().gridsearch(X, y)
+
+plt.hist(faithful(return_X_y=False)['eruptions'], bins=200, color='k');
+plt.plot(X, gam.predict(X), color='r')
+plt.title('Best Lambda: {0:.2f}'.format(gam.lam[0][0]));
+
+
+
+######################################################
+# regression
+
+from pygam import GAM
+from pygam.datasets import trees
+
+X, y = trees(return_X_y=True)
+X.shape
+X
+y
+
+
+gam = GAM(distribution='gamma', link='log')
+gam.gridsearch(X, y)
+
+plt.scatter(y, gam.predict(X))
+plt.xlabel('true volume')
+plt.ylabel('predicted volume')
+
 
 
