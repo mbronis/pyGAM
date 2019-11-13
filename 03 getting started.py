971e170e6e5f73dc9925154b5882c8e21657951a
mbronis <michal.bronis@gmail.com>
2019-11-13 20:32:45 +0100

    fix 3d plot step1

--- 03 getting started.py
+++ 03 getting started.py
@@ -17,12 +17,62 @@ target_df = pd.Series(data.target)
 df.describe()
 
 
-
 #building simple logistic regression
-
 X = df[['mean radius', 'mean texture', 'mean perimeter', 'mean area','mean smoothness', 'mean compactness']]
 y = target_df
 
 #Fit a model with the default parameters
 gam = LogisticGAM().fit(X, y)
 gam.summary()
+
+# describe each term
+from pygam import generate_X_grid
+
+XX = gam.generate_X_grid()
+plt.rcParams['figure.figsize'] = (28, 8)
+fig, axs = plt.subplots(1, len(data.feature_names[0:6]))
+titles = data.feature_names
+for i, ax in enumerate(axs):
+    pdep, confi = gam.partial_dependence(XX, feature=i+1, width=.95)
+    ax.plot(XX[:, i], pdep)
+    ax.plot(XX[:, i], confi[0][:, 0], c='grey', ls='--')
+    ax.plot(XX[:, i], confi[0][:, 1], c='grey', ls='--')
+    ax.set_title(titles[i])plt.show()
+
+
+#my take
+
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+plt.ion()
+plt.rcParams['figure.figsize'] = (28, 8)
+
+fig, axs = plt.subplots(1, len(data.feature_names[0:6]))
+titles = data.feature_names
+for i, ax in enumerate(axs):
+    XX=gam.generate_X_grid(term=i+1, meshgrid=True)
+    pdep, confi = gam.partial_dependence(term=i+1, X=XX, meshgrid=True, width=.95)
+    ax.plot(XX[:, i], pdep)
+    ax.plot(XX[:, i], confi[0][:, 0], c='grey', ls='--')
+    ax.plot(XX[:, i], confi[0][:, 1], c='grey', ls='--')
+    ax.set_title(titles[i])
+    
+plt.show()
+
+XX
+Z = gam.partial_dependence(term=1, X=XX, meshgrid=True)
+#prev 3d plot
+
+plt.ion()
+plt.rcParams['figure.figsize'] = (12, 8)
+
+XX = gam.generate_X_grid(term=1, meshgrid=True)
+Z = gam.partial_dependence(term=1, X=XX, meshgrid=True)
+
+ax = plt.axes(projection='3d')
+ax.plot_surface(XX[0], XX[1], Z, cmap='viridis')
+
+
+
+
