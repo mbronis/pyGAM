9c9a4bf53972c129142a9f6dee9f959e60d87be3
mbronis <michal.bronis@gmail.com>
2019-11-14 07:50:46 +0100

    pre fix for data load

--- 03 getting started.py
+++ 03 getting started.py
@@ -5,9 +5,16 @@
 
 import pandas as pd        
 from pygam import LogisticGAM
+
+from sklearn.metrics import roc_auc_score
 from sklearn.datasets import load_breast_cancer
 
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+#-----------------------------------------------------
 #load the breast cancer data set
+
 data = load_breast_cancer()
 
 #keep first 6 features only
@@ -16,18 +23,17 @@ selected_features = ['mean radius', 'mean texture', 'mean perimeter', 'mean area
 X = pd.DataFrame(data.data, columns=data.feature_names)[selected_features]
 y = pd.Series(data.target)
 
+#-----------------------------------------------------
 #Fit a model with the default parameters
 gam = LogisticGAM().fit(X, y)
 gam.summary()
 
-from sklearn.metrics import roc_auc_score
 roc_auc_score(y,gam.predict_proba(X)) #0.994173140954495
 gam.accuracy(X, y) #0.9560632688927944
 
-# plot each term
+#-----------------------------------------------------
+# Explore and interpret individual features
 
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
 
 plt.ion()
 plt.rcParams['figure.figsize'] = (28, 8)
@@ -43,3 +49,52 @@ for i, ax in enumerate(axs):
     ax.set_title(selected_features[i])
     
 plt.show()
+
+
+#-----------------------------------------------------
+# Tuning Smoothness and Penalties
+
+
+n_splines = [25, 6, 25, 25, 6, 4] 
+lambda_ = 0.6
+constraints = None
+
+gam2 = LogisticGAM(
+    constraints=constraints
+    ,lam=lambda_
+    ,n_splines=n_splines).fit(X, y)
+gam2.summary()
+
+roc_auc_score(y,gam2.predict_proba(X)) #0.991015274034142
+gam2.accuracy(X, y) #0.9507908611599297
+
+
+plt.ion()
+plt.rcParams['figure.figsize'] = (28, 8)
+
+fig, axs = plt.subplots(1, X.shape[1])
+
+for i, ax in enumerate(axs):
+    XX=gam2.generate_X_grid(term=i, meshgrid=True)
+    pdep, confi = gam2.partial_dependence(term=i, X=XX, meshgrid=True, width=.95)
+    ax.plot(XX[0], pdep)
+    ax.plot(XX[0], confi[:, 0], c='grey', ls='--')
+    ax.plot(XX[0], confi[:, 1], c='grey', ls='--')
+    ax.set_title(selected_features[i])
+    
+plt.show()
+
+
+#-----------------------------------------------------
+# Grid search with pyGAM
+
+
+#default in pyGAM grid search is lambda space of {'lam':np.logspace(-3,3,11)}
+
+from pygam import s
+
+gam3 = LogisticGAM(s(0)+s(1))
+gam3.gridsearch(X, y)
+
+X
+y
